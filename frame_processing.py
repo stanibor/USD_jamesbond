d1be37c877155da5a247c3477f746db61888bce4
stanibor <stani.bork@gmail.com>
2021-01-23 17:29:22 +0100

    simple frame processor

--- frame_processing.py
+++ frame_processing.py
@@ -1,3 +1,15 @@
+import numpy as np
+import cv2 as cv
+
 class FramePreprocessor:
-    def __init__(self, ):
-        pass
+    def __init__(self, paddings=(0, 0, 0, 0), size=(84, 84), color_weights=(0.33, 0.33, 0.33)):
+        self.paddings = paddings
+        self.size = size
+        self.color_weights = np.array(color_weights)
+
+    def process(self, frame):
+        out = frame[self.paddings[0]:-(self.paddings[1]+1), self.paddings[1]:-(self.paddings[3]+1), :] * (1.0 / 255.0)
+        out = out.dot(self.color_weights)
+        out = cv.resize(out, self.size)
+        return out
+
--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import gym
-import time
-import numpy as np
 import cv2
+from frame_processing import FramePreprocessor
 
 FPS = 120
 
@@ -20,15 +19,14 @@ if __name__ == '__main__':
     cv2.namedWindow('robot', cv2.WINDOW_NORMAL)
     cv2.resizeWindow('robot', 600, 600)
 
+    preprocessor = FramePreprocessor(paddings=(34, 8, 10, 0), size=(80, 80), color_weights=(0.1, 0.8, 0.1))
+
     while not done:
         action = env.action_space.sample()
         step, reward, done, info = env.step(action)
 
-
-        blue = step[38:-20, 8:, :]/255
-        blue = blue.dot([0.1, 0.8, 0.1])
-        # print(blue.shape)
-        small = cv2.resize(blue, (76, 76))
-        cv2.imshow('game', blue)
-        cv2.imshow('robot', small)
+        frame = step
+        robot = preprocessor.process(frame)
+        cv2.imshow('game', frame)
+        cv2.imshow('robot', robot)
         cv2.waitKey(int(1000/FPS))
